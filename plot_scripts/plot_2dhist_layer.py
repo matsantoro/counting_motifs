52dd68fb2af870595901b0e3c824c94bba19a7b3
matsantoro <matteo.santoro@epfl.ch>
2021-01-02 11:17:33 -0500

    Changed 2d hist images

--- plot_scripts/plot_2dhist_layer.py
+++ plot_scripts/plot_2dhist_layer.py
@@ -17,35 +17,35 @@ for dimension in range(1,7):
     bsdf -= np.diag(np.diag(bsdf))/2
     edgedf -= np.diag(np.diag(edgedf))/2
     morphs = [elem if elem != "L2" else "L23" for elem in morphs]
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(esdf, xticklabels = morphs, yticklabels = morphs, cmap = 'viridis')
     ax.set_title("Extended simplices count per layer on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("ESlayer2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(bsdf, xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bisimplices count per layer on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BSlayer2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(edgedf, xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bidirectional edge count per layer on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BElayer2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(np.divide(bsdf,edgedf), xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bisimplex count over bidirectional edge count per layer on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BSBElayer2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(np.divide(esdf,edgedf), xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("E. simplex count over bidirectional edge count per layer on dim "+ str(dimension + 1))
@@ -56,47 +56,47 @@ for dimension in range(1,7):
     bsdf[np.isnan(bsdf)] = 0
     edgedf[np.isnan(edgedf)] = 0
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.sum(esdf, axis = 1), tick_label = morphs)
     ax.set_title("E. simplex sink count per layer on dim " + str(dimension +1))
     ax.set_yscale("log")
     fig.savefig("ESsinklayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # sink
     ax.bar(range(len(morphs)), np.sum(edgedf, axis = 1), tick_label = morphs)
     ax.set_title("Bidirectional edge count per layer on dim " + str(dimension +1))
     ax.set_yscale("log")
     fig.savefig("BElayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # end
     ax.bar(range(len(morphs)), np.sum(esdf, axis = 0), tick_label = morphs)
     ax.set_title("E. simplex end count per layer on dim " + str(dimension +1))
     ax.set_yscale("log")
     fig.savefig("ESendlayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # end
     ax.bar(range(len(morphs)), np.sum(bsdf, axis = 0), tick_label = morphs)
     ax.set_title("Bisimplex sink count per layer on dim " + str(dimension +1))
     ax.set_yscale("log")
     fig.savefig("BSsinklayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # end
     ax.bar(range(len(morphs)), np.divide(np.sum(bsdf, axis = 0), np.sum(edgedf, axis = 0)), tick_label = morphs)
     ax.set_title("Bisimplex sink over bidegree counts per layer on dim " + str(dimension +1))
     fig.savefig("BSsinkBElayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # end
     ax.bar(range(len(morphs)), np.divide(np.sum(esdf, axis = 1), np.sum(edgedf, axis = 0)), tick_label = morphs)
     ax.set_title("Extended simplex sink over bidegree counts per layer on dim " + str(dimension +1))
     fig.savefig("ESsinkBElayerhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot() # end
     ax.bar(range(len(morphs)), np.divide(np.sum(esdf, axis = 0), np.sum(edgedf, axis = 0)), tick_label = morphs)
     ax.set_title("Extended simplex end over bidegree counts per layer on dim " + str(dimension +1))
--- plot_scripts/plot_2dhist_morph.py
+++ plot_scripts/plot_2dhist_morph.py
@@ -16,35 +16,35 @@ for dimension in range(1,7):
     edgedf[edgedf == 0] = np.nan
     edgedf -= np.diag(np.diag(edgedf))/2
     bsdf -= np.diag(np.diag(bsdf))/2
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(esdf, xticklabels = morphs, yticklabels = morphs, cmap = 'viridis')
     ax.set_title("Extended simplices count per mtype on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("ESmtype2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(bsdf, xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bisimplices count per mtype on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BSmtype2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(edgedf, xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bidirectional edge count per mtype on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BEmtype2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(np.divide(bsdf,edgedf), xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("Bisimplex count over bidirectional edge count per mtype on dim "+ str(dimension + 1))
     ax.set_xticklabels(ax.get_xticklabels(), rotation = 45, ha = 'right')
     fig.savefig("BSBEmtype2dhistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 13])
+    fig = plt.figure(figsize = [7, 7])
     ax = fig.add_subplot()
     sns.heatmap(np.divide(esdf,edgedf), xticklabels = morphs, yticklabels = morphs,cmap= 'viridis')
     ax.set_title("E. simplex count over bidirectional edge count per mtype on dim "+ str(dimension + 1))
@@ -55,43 +55,43 @@ for dimension in range(1,7):
     bsdf[np.isnan(bsdf)] = 0
     edgedf[np.isnan(edgedf)] = 0
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.sum(esdf, axis = 1), tick_label = morphs)
     ax.set_title("E. simplex sink count per mtype on dim " + str(dimension +1))
     fig.savefig("ESsinkmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.sum(edgedf, axis = 1), tick_label = morphs)
     ax.set_title("Bidirectional edge count per mtype on dim " + str(dimension +1))
     fig.savefig("BEmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.sum(esdf, axis = 0), tick_label = morphs)
     ax.set_title("E. simplex end count per mtype on dim " + str(dimension +1))
     fig.savefig("ESendmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.sum(bsdf, axis = 0), tick_label = morphs)
     ax.set_title("Bisimplex sink count per mtype on dim " + str(dimension +1))
     fig.savefig("BSsinkmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.divide(np.sum(bsdf, axis = 0), np.sum(edgedf, axis = 0)), tick_label = morphs)
     ax.set_title("Bisimplex sink over bidegree counts per mtype on dim " + str(dimension +1))
     fig.savefig("BSsinkBEmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.divide(np.sum(esdf, axis = 1), np.sum(bsdf, axis = 0)), tick_label = morphs)
     ax.set_title("Extended simplex sink over bidegree counts per mtype on dim " + str(dimension +1))
     fig.savefig("ESsinkBEmtypehistD" + str(dimension+1), facecolor = "white")
 
-    fig = plt.figure(figsize = [13, 7])
+    fig = plt.figure(figsize = [8, 6])
     ax = fig.add_subplot()
     ax.bar(range(len(morphs)), np.divide(np.sum(esdf, axis = 0), np.sum(bsdf, axis = 0)), tick_label = morphs)
     ax.set_title("Extended simplex end over bidegree counts per mtype on dim " + str(dimension +1))
