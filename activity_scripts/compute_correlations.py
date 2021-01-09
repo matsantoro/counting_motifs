20b109cf3cc937abc53b3990e4da071956ee5b4a
matsantoro <matteo.santoro@epfl.ch>
2021-01-09 06:33:49 -0500

    Changed correlation computation

--- activity_scripts/compute_correlations.py
+++ activity_scripts/compute_correlations.py
@@ -1,18 +1,15 @@
-from robust_motifs.counting import correlations_maximal_simplex
+from robust_motifs.counting import correlations_simplexwise
 from robust_motifs.data import load_sparse_matrix_from_pkl
 from pathlib import Path
 import pickle
+import numpy as np
 
 data = Path("data/activity/network/")
-spine_files = sorted(list(data.glob("**/*maximal*spine*.pkl")))
-end_files = sorted(list(data.glob("**/*maximal*end*.pkl")))
-any_files = sorted(list(data.glob("**/*maximal*any*.pkl")))
-spontaneous_file_list = list(Path("data/activity/spikes/spont").glob("*"))
-conn_matrix = load_sparse_matrix_from_pkl("data/activity/network/cons_locs_pathways_mc2_Column/cons_locs_pathways_mc2_Column.pkl")
-for elem in spontaneous_file_list:
-    print(elem)
-    gids = pickle.load(open(elem/ "gids.pkl", 'rb'))
-    matrix = pickle.load(open(elem/ "correlation.pkl", 'rb'))
-    for result in correlations_maximal_simplex(spine_files, gids, 62693, 95038, matrix, conn_matrix,'sspine'):
-        print(len(result[0]))
-        print(len(result[1]))
+matrix = load_sparse_matrix_from_pkl(data/ "cons_locs_pathways_mc2_Column/cons_locs_pathways_mc2_Column.pkl")
+matrix = np.array(matrix.todense())
+for trace in Path("data/activity/spikes/spont").glob("*"):
+    gids = pickle.load(open(trace/"gids.pkl",'rb'))
+    corr_matrix = pickle.load(open(trace/"pearson_correlation.pkl",'rb'))
+    a = correlations_simplexwise(data/ "cons_locs_pathways_mc2_Column/cons_locs_pathways_mc2_Column-count-maximal.h5",
+                             gids, 62693, 94038, corr_matrix, matrix, "all")
+    pickle.dump(a, open(trace / "simplexwise_pc_all.pkl", "wb"))
