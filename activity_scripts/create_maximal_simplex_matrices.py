6ad4b704cac7a40936861675c4147f0686d3295d
matsantoro <matteo.santoro@epfl.ch>
2021-02-02 14:10:22 +0100

    Renamed notebook files + removed unusued script

--- activity_scripts/create_maximal_simplex_matrices.py
+++
@@ -1,4 +0,0 @@
-from robust_motifs.counting import maximal_matrices_from_file
-from pathlib import Path
-
-maximal_matrices_from_file(Path("data/activity/network/cons_locs_pathways_mc2_Column/cons_locs_pathways_mc2_Column.pkl"))
