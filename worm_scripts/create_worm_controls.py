8ff4962514f5c797b26ef857e1c8a56c9e9cfcd9
matsantoro <matteo.santoro@epfl.ch>
2021-04-15 09:27:08 -0400

    Added celegans control creation

---
+++ worm_scripts/create_worm_controls.py
@@ -0,0 +1,18 @@
+from robust_motifs.data import create_control_graphs_from_matrix
+from pathlib import Path
+
+create_control_graphs_from_matrix(10, Path("data/worm/full/graph.pkl"),
+	Path("data/worm_control/bishuffled"),
+	type = "shuffled_biedges",
+	seed = 1)
+
+create_control_graphs_from_matrix(10, Path("data/worm/full/graph.pkl"),
+	Path("data/worm_control/underlying"),
+	type = "underlying",
+	seed = 1)
+
+create_control_graphs_from_matrix(10, Path("data/worm/full/graph.pkl"),
+	Path("data/worm_control/AER"),
+	type = "adjusted",
+	seed = 1)
+
