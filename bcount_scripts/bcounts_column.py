e3d1444edad7ba4245b707216a78b08c7d0f19db
matsantoro <matteo.santoro@epfl.ch>
2021-04-19 03:54:03 -0400

    Fixed some scripts, finalized worm notebook

--- bcount_scripts/bcounts_column.py
+++ bcount_scripts/bcounts_column.py
@@ -1,4 +1,4 @@
 from robust_motifs.counting import bcount_from_file
 from pathlib import Path
 
-bcount_from_file(Path("data/bcounts/column/cons_locs_pathways_mc0_Column.pkl"), 10)
+bcount_from_file(Path("data/bcounts/column/seed_0/cons_locs_pathways_mc0_Column.pkl"), 10)
--- bcount_scripts/create_maximal_count.py
+++ bcount_scripts/create_maximal_count.py
@@ -1,4 +1,4 @@
 from robust_motifs.data import create_maximal_simplex_file
 from pathlib import Path
 
-create_maximal_simplex_file(Path("data/bcounts/adjusted/graph-count.h5"),Path("data/bcounts/adjusted/graph-count-maximal.h5"), True)
+create_maximal_simplex_file(Path("data/bcounts/column/seed_0/cons_locs_pathways_mc0_Column-count.h5"),Path("data/bcounts/column/seed_0/cons_locs_pathways_mc0_Column-count-maximal.h5"), True)
